5e4dec2346d0e6475992d740ec2a941f04ee1f9c
Justin Kindrix <jkindrix@gmail.com>
2020-04-23 00:18:52 -0500

    Remove PingPong functionality

--- events/message.js
+++ events/message.js
@@ -1,10 +1,6 @@
 const kick = require("../commands/kick")
 
 module.exports = (client, message) => {
-  if (message.content === "ping") {
-    message.reply("Pong!");
-  }
-
   if (message.content.startsWith("!kick")) {
     return kick(message)
   }
